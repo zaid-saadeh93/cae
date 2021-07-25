165927d24c475be5e3f22d7439942270dee26086
ZaidSaadeh <>
2021-07-25 12:33:46 +0000

    added new info

---
+++ test.js
@@ -0,0 +1,44 @@
+var age-parseInt(prompt('who old are you'));
+
+/*
+function ageplusten(){
+  var c = age+10;
+
+  return c ;
+}
+*/
+var ageplusten = function(ageInput) {
+  var c = ageInput+10;
+  return c ;
+}
+
+var result = parseInt(ageplusten(ag))
+
+var agebefore = ageplusten () -5
+alert(agebefore);
+
+
+
+
+
+
+
+
+
+
+
+
+var num1 = parseInt (prompt('Enter the first num'))
+var num2 = parseInt (prompt('Enter the second num'))
+
+
+function sum (x,y){
+  return x+y ;
+
+}
+
+var result = sum(1,5);
+var result2 = sum(num1,num2);
+
+
+console.log (result);
