fc06194c43e6c1e00a20b085be753272542bc954
ZaidSaadeh <>
2021-07-25 14:46:24 +0000

    added new info

--- wep.js
+++ wep.js
@@ -1,16 +1,27 @@
+
 var name = prompt (" What is ypur name? ")
 alert ("Hello " + name + " Welcom to my website ")
 
 var age = prompt (" How old are you? ")
 
-var bestcar = prompt (" What is the best car in your opinion? , mercedes, mustang, BMW ")
+var bestcar = prompt (" What is the best car in your opinion? mercedes, mustang")
 
-while(bestcar != "mercedes" && bestcar != "mustang" &&  bestcar != "BMW"){
+while(bestcar != "mercedes" && bestcar != "mustang")
+  bestcar= prompt (" What is the best car in your opinion? mercedes, mustang")
 
-  bestcar = prompt (" What is the best car in your opinion? , mercedes, mustang, BMW")
-if (bestcar == "mercedes" ||   bestcar == "mustang"  ){
-  alert ("good choice ! " + name )
+
+
+
+if (bestcar == "mercedes" ||   bestcar == "mustang"  )
+{var numberofpic = prompt(" how many times you like to see the pic ")
+
+
+  
+  {
+    alert ("good choice ! " + name )
+  }
 }
+
 else{
   alert ("You're an awesome person")
 }
@@ -18,6 +29,5 @@ else{
 console.log(name)
 
 document.write(" thank you " + name )
-
 
 
